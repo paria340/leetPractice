726716c3d9979bbad744e7555cdce34102e9ee35
paria340 <paria340@gmail.com>
2021-12-14 22:16:56 -0800

    day3

--- scripts.js
+++ scripts.js
@@ -86,3 +86,22 @@ const romanToInt = function(string){
 }
 
 console.log(romanToInt(string))
+
+// const hello = user => {
+//     console.log(`Hello ${user}`);
+//   }
+  
+// hello();
+array = ["flower","flow","flight"]
+const longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return '';
+    let r = '';
+    for (let i = 0; i < strs[0].length; i++) {
+      for (let j = 1; j < strs.length; j++) {
+        if (strs[0][i] !== strs[j][i]) return r;
+      }
+      r += strs[0][i];
+    }
+    return r;
+  };
+console.log(longestCommonPrefix(array))
